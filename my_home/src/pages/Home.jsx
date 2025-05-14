c51fe2908403e6e5e34b52d9d7d0991035204b0c
jojayeon <jojayeon6152@gmail.com>
2025-05-14 16:59:47 +0900

    :art: 메인 페이지 수정, 상단 메뉴 추가

--- my_home/src/pages/Home.jsx
+++ my_home/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+// src/pages/Home.jsx
+import { Link } from 'react-router-dom';
+import '../styles/Home.css'
+
 function Home() {
   return (
-    <div>
-      <h1>Home 페이지입니다</h1>
+    <div className="home-wrapper">
+      <h1>자 연</h1>
+      <div className="home-buttons">
+        <Link to="/page1">page1</Link>
+        <Link to="/page2">page2</Link>
+        <Link to="/page3">page3</Link>
+      </div>
     </div>
   );
 }
