5f46bcbe83a53fc20eb0bd2c3a6e11b94ea1ac6a
jojayeon <jojayeon6152@gmail.com>
2025-05-16 16:49:16 +0900

    :sparkles: 버튼추가, 테스트용 글 작성

--- my_home/src/pages/nature.jsx
+++ my_home/src/pages/nature.jsx
@@ -1,16 +1,23 @@
 //마지막 마무리 페이지
 //숲나무 사람이 같이 있는 이미지를 보여주고 글로 조자연이라는 것을 말해주면서 마무리 
 //처음으로 돌아가는 버튼 
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
+import { useNavigate } from 'react-router-dom';
+import '../styles/nature.css'; // ✨ 새 CSS 파일 추가 예정
 
-function Page3() {
+function Nature() {
+  const navigate = useNavigate();
+
   return (
     <Layout>
-      <div>
-        <h1>Page3 내용</h1>
+      <div className="nature-container">
+        <h1>조자연입니다.</h1>
+        <button className="return-button" onClick={() => navigate('/')}>
+          처음
+        </button>
       </div>
     </Layout>
   );
 }
 
-export default Page3;
+export default Nature;
