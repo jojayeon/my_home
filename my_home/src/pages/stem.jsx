1d71335e1fb637e3e9355b6d75bbd8b0471e47aa
jojayeon <jojayeon6152@gmail.com>
2025-05-16 16:48:41 +0900

    :sparkles: 버튼추가, 테스트용 글 작성

--- my_home/src/pages/stem.jsx
+++ my_home/src/pages/stem.jsx
@@ -1,14 +1,28 @@
 //자기소개하는 페이지
 //내 기본적인 스펙과 이야기를 적을 것
 import Layout from '../components/Layout';
-function Page2() {
+import { useNavigate } from 'react-router-dom';
+import '../styles/stem.css'; // ✨ 새로운 CSS 파일 추가 예정
+
+function Stem() {
+  const navigate = useNavigate();
+
   return (
     <Layout>
-      <div>
-        <h1>Page2 내용</h1>
+      <div className="stem-container">
+        <div className="intro-box">
+          <h1>안녕하세요, 저는 조자연입니다.</h1>
+          <p>
+            저는 자연과 같은 조화를 추구하며, 기술과 감성을 잇는 개발자가 되고 싶습니다. <br />
+            이 포트폴리오는 저의 생각과 정체성을 자연이라는 이야기로 풀어냈습니다.
+          </p>
+        </div>
+        <button className="next-button" onClick={() => navigate('/nature')}>
+          다음
+        </button>
       </div>
     </Layout>
   );
 }
 
-export default Page2;
+export default Stem;
