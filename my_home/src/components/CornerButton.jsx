f76955c00cb3398a4f5745bb701a5a7ee29f1d0c
jojayeon <jojayeon6152@gmail.com>
2025-05-21 22:09:37 +0900

    :art: 기본형으로 변경

--- my_home/src/components/CornerButton.jsx
+++ my_home/src/components/CornerButton.jsx
@@ -1,27 +1,19 @@
 // components/CornerButton.jsx
 // CornerButton.jsx
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/CornerButton.css"; // 아래 스타일을 여기에 작성
 
 const CornerButton = ({ to }) => {
-  const [isClicked, setIsClicked] = useState(false);
   const navigate = useNavigate();
 
   const handleClick = () => {
-    setIsClicked(true);
-    setTimeout(() => {
-      navigate(to);
-    }, 1500); // 애니메이션 지속시간
+    navigate(to);
   };
 
   return (
-    <div className={`corner-container ${isClicked ? "clicked" : ""}`}>
-      <div className="cover-box" />
-      <button className="corner-button" onClick={handleClick}>
-        다음
-      </button>
-    </div>
+    <button onClick={handleClick} className="corner-button">
+      다음
+    </button>
   );
 };
 
