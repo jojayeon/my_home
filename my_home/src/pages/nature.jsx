77c7b2cde7d9b7c4e5abebfcce0508d661e92cc9
jojayeon <jojayeon6152@gmail.com>
2025-05-27 22:05:20 +0900

    :lipstick: 레이 아웃 제거

--- my_home/src/pages/nature.jsx
+++ my_home/src/pages/nature.jsx
@@ -1,20 +1,17 @@
 //마지막 마무리 페이지
 //숲나무 사람이 같이 있는 이미지를 보여주고 글로 조자연이라는 것을 말해주면서 마무리 
 //처음으로 돌아가는 버튼 
-import Layout from '../components/Layout';
 import '../styles/nature.css'; // ✨ 새 CSS 파일 추가 예정
 import CornerButton from '../components/CornerButton';
 
 function Nature() {
 
   return (
-    <Layout>
-      <div className="nature-container">
-        <h1>제 조자연입니다.</h1>
-        <h1>"자연"이라는 단어를 보고 이제는 저를 봐주세요.</h1>
-          <CornerButton to="/"/>
-      </div>
-    </Layout>
+    <div className="nature-container">
+      <h1>제 이름은 조자연입니다.</h1>
+      <h1>"자연"이라는 단어를 보고 이제는 저를 봐주세요.</h1>
+        <CornerButton to="/"/>
+    </div>
   );
 }
 
