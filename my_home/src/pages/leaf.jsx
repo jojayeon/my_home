af43cb881f9f69dc5e1edc4995aa733017f6c7b6
jojayeon <jojayeon6152@gmail.com>
2025-07-11 16:36:46 +0900

    :package: 순서 변경및 링크 변경

--- my_home/src/pages/leaf.jsx
+++ my_home/src/pages/leaf.jsx
@@ -6,6 +6,49 @@ import '../styles/leaf.css';
 function Leaf() {
   const projects = [
     {
+      title: "fps연습\n(개인 프로젝트)",
+      subtitle: '에임 연습',
+      description: [
+        '오버워치라는 게임을 즐기면서 너무 빠지는 것을 방지하고',
+        '실력은 좋아졌으면 하는 마음에 만든 프로젝트입니다.',
+      ],
+      image: '/images/aim.jpg',
+      links: [
+        {
+          label: '바로가기',
+          href: 'https://aim-practice-3-d.vercel.app/',
+        },
+        {
+          label: '깃허브 바로가기',
+          href: 'https://github.com/jojayeon/Aim_Practice',
+        },
+        {
+          label: '노션 바로가기',
+          href: 'https://www.notion.so/1f14d9dd1326800e9505e8a9cf6ff65e',
+        },
+      ],
+    },
+    {
+      title: '대전 맛집 지도\n(개인 프로젝트)',
+      subtitle: '대전 맛집',
+      description: [
+        '데이터 분석을 중점으로 두고 진행한 프로젝트입니다.',
+        '블루리본과 카카오맵 평점, 리뷰 분석까지',
+        '총 3가지를 중점으로 판단한 맛집 지도.',
+      ],
+      image: '/images/map.jpg',
+      links: [
+        {
+          label: '바로가기',
+          href: 'https://py-really-delicious-restaurant-fs28ehrn1-jojayeons-projects.vercel.app/',
+        },
+        {
+          label: '깃허브 바로가기',
+          href: 'https://github.com/jojayeon/py-Really_delicious_restaurant',
+        },
+      ],
+    },
+    {
       title: `RockCoders\n(팀 프로젝트)`,
       subtitle: 'ERP 프로그램',
       description: [
@@ -48,66 +91,22 @@ function Leaf() {
       ],
     },
     {
-      title: '대전 맛집 지도\n(개인 프로젝트)',
-      subtitle: '대전 맛집',
-      description: [
-        '데이터 분석을 중점으로 두고 진행한 프로젝트입니다.',
-        '블루리본과 카카오맵 평점, 리뷰 분석까지',
-        '총 3가지를 중점으로 판단한 맛집 지도.',
-      ],
-      image: '/images/map.jpg',
-      links: [
-        {
-          label: '바로가기',
-          href: 'https://py-really-delicious-restaurant-fs28ehrn1-jojayeons-projects.vercel.app/',
-        },
-        {
-          label: '깃허브 바로가기',
-          href: 'https://github.com/jojayeon/py-Really_delicious_restaurant',
-        },
-      ],
-    },
-    {
-      title: "'나'인공지능\n(개인 프로젝트)",
+      title: "'청소 방법'인공지능\n(개인 프로젝트)",
       subtitle: 'LLama 파인 튜닝',
       description: [
-        '제가 작성했던 노션을 가지고 학습을 하여',
-        '나와 같은 인공지능을 만들어보려했습니다.',
-        '머신러닝과 파인튜닝의 학습부족으로',
-        '결과적으로 완성하지는 못한 프로젝트입니다.',
+        '청소에 관심이 많아, 이미 알고 있는 내용도 정확하게',
+        '다시 확인하고 싶을 때 빠르게 도움을 받을 수 있는',
+        '청소법을 알려주는 AI를 개발하는 것이 목표입니다.'
       ],
       image: '/images/llama.jpg',
       links: [
         {
-          label: '바로가기',
-          href: 'https://myaillama.s3.ap-northeast-2.amazonaws.com/index.html',
+          label: '진행 중',
+          href: '',
         },
         {
           label: '깃허브 바로가기',
-          href: 'https://github.com/jojayeon/Fine_Tuning',
-        },
-      ],
-    },
-    {
-      title: "fps연습\n(개인 프로젝트)",
-      subtitle: '에임 연습',
-      description: [
-        '오버워치라는 게임을 즐기면서 너무 빠지는 것을 방지하고',
-        '실력은 좋아졌으면 하는 마음에 만든 프로젝트입니다.',
-      ],
-      image: '/images/aim.jpg',
-      links: [
-        {
-          label: '바로가기',
-          href: 'https://aim-practice-3-d.vercel.app/',
-        },
-        {
-          label: '깃허브 바로가기',
-          href: 'https://github.com/jojayeon/Aim_Practice',
-        },
-        {
-          label: '노션 바로가기',
-          href: 'https://www.notion.so/1f14d9dd1326800e9505e8a9cf6ff65e',
+          href: 'https://github.com/jojayeon/llama_Fine_Tuning',
         },
       ],
     },
